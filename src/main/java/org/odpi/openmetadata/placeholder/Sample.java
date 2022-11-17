84b17d0860299062ba18c0de4c7675da8cfd7364
Nigel Jones <nigel.l.jones+git@gmail.com>
2022-11-17 13:54:17 +0000

    Initial commit

---
+++ src/main/java/org/odpi/openmetadata/placeholder/Sample.java
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright Contributors to the ODPi Egeria project. */
+
+package org.odpi.openmetadata.placeholder;
+
+/**
+ * TODO: Sample has no implementation - you need to replace this code
+ */
+public class Sample {
+}
